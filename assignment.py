f5ab35a97f51d764e50e2c11119c20cd019d72ce
spersat <spersat@alten.be>
2020-08-18 14:32:48 +0200

    a

---
+++ assignment.py
@@ -0,0 +1,6 @@
+a=5
+a-=2 # a = a-2 ==> as a=5 at the beginning, now a=3
+print(a)
+b=10
+b-=30 # as a=10 at the beginning, now a=a-30 so 10-30 result: now a=-20
+print(b)
---
+++ identificator.py
@@ -0,0 +1,14 @@
+#identificator
+a=5
+b=5
+print(id(a))
+print(id(b)) # id(b) is the same as id(a)
+
+#identificator lists: ids are differents even if content is the same
+list1 = [1,2,3]
+list2 = [1,2,3]
+list3 = list1
+
+print(id(list1))
+print(id(list2)) # id(list1) differen than id(list2)
+print(id(list3)) # id(list3) same as id(list1)
