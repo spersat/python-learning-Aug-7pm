f23e2a8b2b02840ba0dbfaa9111a3b4d3bcc94b4
spersat <spersat@alten.be>
2020-08-18 16:26:41 +0200

    f

---
+++ unit_binary_ternary.py
@@ -0,0 +1,18 @@
+#unitary, binary, ternary operators
+#unitary
+a=5
+
+#binary
+a=5
+b=6
+c=a+b #binary operator operand1 + operand2
+
+#ternary
+
+a = 20
+b = 30
+c = 40
+d = 50
+
+e = d if  a>b else c # Syntax: "First Operand" if "conditon (or second operand)" else  "third operand"
+print(e) # a>b is false in the example, e=c 
